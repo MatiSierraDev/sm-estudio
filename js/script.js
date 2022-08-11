0a83a600921a85f004c634a2ba27a958cfb65bef
matiraw <matisierra.develop@gmail.com>
2022-08-11 13:20:03 -0300

    Add image section contact 🚗

--- js/script.js
+++ js/script.js
@@ -1,120 +1,131 @@
-const $ = (selector) => document.querySelector(`${selector}`);
+window.addEventListener('DOMContentLoaded', () => {
 
-// let $menu = $('.menu');
-// let $menuBtn = $('.menu-btn');
-// $menuBtn.addEventListener('click', () => {
-//   const $hamburgerToggle = $menuBtn.getElementsByTagName('svg').item(0)
-//   const $hamburgerToggleOpen = $menuBtn.getElementsByTagName('svg').item(1)
-//   $hamburgerToggle.classList.toggle('none')
-//   $hamburgerToggleOpen.classList.toggle('none')
-//   $menu.classList.toggle('is-active')
-// });
+  window.addEventListener('load', () => {
 
-//funcion autoejecutable para evitar colisiones
-//MENU
+    const $ = (selector) => document.querySelector(`${selector}`);
 
-/**
- * funcion autoejecutable que maneja el menu
- * @params{string} nodo raiz document
- * @params{string} funcion que recibe un selector
- */
+    // let $menu = $('.menu');
+    // let $menuBtn = $('.menu-btn');
+    // $menuBtn.addEventListener('click', () => {
+    //   const $hamburgerToggle = $menuBtn.getElementsByTagName('svg').item(0)
+    //   const $hamburgerToggleOpen = $menuBtn.getElementsByTagName('svg').item(1)
+    //   $hamburgerToggle.classList.toggle('none')
+    //   $hamburgerToggleOpen.classList.toggle('none')
+    //   $menu.classList.toggle('is-active')
+    // });
 
-((d, $) => {
-  let $menu = $('.menu');
-  let $menuBtn = $('.menu-btn');
+    //funcion autoejecutable para evitar colisiones
+    //MENU
 
-  $menuBtn.addEventListener('click', () => {
-    const $hamburgerToggle = $menuBtn.getElementsByTagName('svg').item(0)
-    const $hamburgerToggleOpen = $menuBtn.getElementsByTagName('svg').item(1)
+    /**
+     * funcion autoejecutable que maneja el menu
+     * @params{string} nodo raiz document
+     * @params{string} funcion que recibe un selector
+     */
 
-    $hamburgerToggle.classList.toggle('none')
-    $hamburgerToggleOpen.classList.toggle('none')
-    $menu.classList.toggle('is-active')
-  });
+    //Menu
+    ((d, $) => {
+      let $menu = $('.menu');
+      let $menuBtn = $('.menu-btn');
 
-  //Delegacion de eventos para optimizar los click de los enlaces === a
-  //delego el evento al elemto padre superior o al nodo raiz (el document) y apartir de ahi busco el selector que coincida(!e.target.matches('.menu a')) para descadenar la programacion
-  d.addEventListener("click", (e) => {
-    //sino coincide con la clase .menu a que retorne falso
-    if (!e.target.matches('.menu a')) return false
+      $menuBtn.addEventListener('click', () => {
+        const $hamburgerToggle = $menuBtn.getElementsByTagName('svg').item(0)
+        const $hamburgerToggleOpen = $menuBtn.getElementsByTagName('svg').item(1)
 
-    const $hamburgerToggle = $menuBtn.getElementsByTagName('svg').item(0)
-    const $hamburgerToggleOpen = $menuBtn.getElementsByTagName('svg').item(1)
+        $hamburgerToggle.classList.toggle('none')
+        $hamburgerToggleOpen.classList.toggle('none')
+        $menu.classList.toggle('is-active')
+      });
 
-    $hamburgerToggle.classList.remove('none')
-    $hamburgerToggleOpen.classList.add('none')
-    $menu.classList.toggle('is-active')
+      //Delegacion de eventos para optimizar los click de los enlaces === a
+      //delego el evento al elemto padre superior o al nodo raiz (el document) y apartir de ahi busco el selector que coincida(!e.target.matches('.menu a')) para descadenar la programacion
+      d.addEventListener("click", (e) => {
+        //sino coincide con la clase .menu a que retorne falso
+        if (!e.target.matches('.menu a')) return false
 
-    //Navlink active cuando se selcciona
-    d.querySelector('a.active-link').classList.remove('active-link')
-    e.target.classList.add('active-link')
-  });
+        const $hamburgerToggle = $menuBtn.getElementsByTagName('svg').item(0)
+        const $hamburgerToggleOpen = $menuBtn.getElementsByTagName('svg').item(1)
 
-})(document, $);
+        $hamburgerToggle.classList.remove('none')
+        $hamburgerToggleOpen.classList.add('none')
+        $menu.classList.toggle('is-active')
 
-((d, $) => {
+        //Navlink active cuando se selcciona
+        d.querySelector('a.active-link').classList.remove('active-link')
+        e.target.classList.add('active-link')
+      });
 
-  const observer = new IntersectionObserver(handleInterceptionObserver, {
-    threshold: 0.20
-  });
+    })(document, $);
 
-  const $scrollTrackerElement = d.getElementById('inicio');
-  observer.observe($scrollTrackerElement);
+    //Social icons
+    ((d, $) => {
 
-  function handleInterceptionObserver(entries) {
-    entries.forEach(entry => {
-      // console.log(entry)
-      const $socialIcons = d.querySelector('.social');
+      const observer = new IntersectionObserver(handleInterceptionObserver, {
+        threshold: 0.20
+      });
 
-      if (!entry.isIntersecting) {
-        $socialIcons.firstElementChild.classList.remove('none');
-        $socialIcons.firstElementChild.classList.add('social-contact');
-      } else {
-        $socialIcons.firstElementChild.classList.remove('social-contact');
-        $socialIcons.firstElementChild.classList.add('none');
+      const $scrollTrackerElement = d.getElementById('inicio');
+      observer.observe($scrollTrackerElement);
+      // debugger;
+
+      function handleInterceptionObserver(entries) {
+        entries.forEach(entry => {
+          // console.log(entry)
+          const $socialIcons = d.querySelector('.social');
+
+          if (!entry.isIntersecting) {
+            $socialIcons.firstElementChild.classList.remove('none');
+            $socialIcons.firstElementChild.classList.add('social-contact');
+          } else {
+            $socialIcons.firstElementChild.classList.remove('social-contact');
+            $socialIcons.firstElementChild.classList.add('none');
+          }
+        });
       }
-    });
-  }
 
-})(document, $);
+    })(document, $);
 
+    //Formulario
+    ((d, $) => {
+      d.addEventListener('submit', (e) => {
+        e.preventDefault();
+        console.log(e)
+        if (e.target.matches('form')) {
 
-//Formulario
-((d, $) => {
-  d.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(e)
-    if (e.target.matches('form')) {
+          const $spinner = $('.form-loader')
+          const $submit = $('.form-container .btn-color')
+          const $labels = d.querySelectorAll('.form-label [required]')
 
-      const $spinner = $('.form-loader')
-      const $submit = $('.form-container .btn-color')
-      const $labels = d.querySelectorAll('.form-label [required]')
+          $submit.style.cursor = 'no-drop';
 
-      $submit.style.cursor = 'no-drop';
-
-      $spinner.classList.remove('none');
+          $spinner.classList.remove('none');
 
 
 
-      $labels.forEach((item) => {
-        const $pattern = item.pattern || item.pattern;
+          $labels.forEach((item) => {
+            const $pattern = item.pattern || item.pattern;
 
-        //create message
-        const $span = d.createElement('span');
-        $span.textContent = item.title;
-        $span.classList.add('form-message-error')
+            //create message
+            const $span = d.createElement('span');
+            $span.textContent = item.title;
+            $span.classList.add('form-message-error')
 
-        item.insertAdjacentElement('afterend', $span)
-        // debugger
+            item.insertAdjacentElement('afterend', $span)
+            // debugger
 
-        //reset form
-        item.value = ''
-      })
+            //reset form
+            item.value = ''
+          })
 
-      setTimeout(() => {
-        $spinner.classList.add('none');
-        $submit.style.cursor = 'pointer';
-      }, 100);
-    }
-  });
-})(document, $);
+          setTimeout(() => {
+            $spinner.classList.add('none');
+            $submit.style.cursor = 'pointer';
+          }, 100);
+        }
+      });
+    })(document, $);
+
+    document.querySelector('html').style.opacity = '1';
+  })
+
+});
